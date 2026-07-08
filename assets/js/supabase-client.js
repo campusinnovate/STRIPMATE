7cd044a4a09f5145fe7b8bc3b43bf07d32d0f85b
campusinnovate <innovatecampus@gmail.com>
2026-07-08 06:13:29 +0000

    update error

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = 'https://jbgzoeggqbbmokwignst.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_VzxQdyc2J8zpVXZE41fBOw_p3AxzVv9'
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
     storageKey: 'stripamate-auth'
