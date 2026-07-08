e452361ca6cafe45814f09cfecf1bb3627665160
campusinnovate <innovatecampus@gmail.com>
2026-07-08 06:21:28 +0000

    update supabase code

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -1,5 +1,6 @@
 const SUPABASE_URL = 'https://jbgzoeggqbbmokwignst.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_VzxQdyc2J8zpVXZE41fBOw_p3AxzVv9'
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
